c9863bfc31066d3ed22139a86de9543d1f9f71bc
Sidharth Shah <sidharth@fafadiatech.com>
2013-01-22 12:03:15 +0530

    now extracting hostnames

--- squid-analyzer/streaming/py/gen_dataset_mapper.py
+++ squid-analyzer/streaming/py/gen_dataset_mapper.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
 import sys 
-
+from urlparse import urlparse
 for eachLine in sys.stdin:
     eachLine = eachLine.strip() 
-    print "\t".join([eachLine.split()[0],eachLine.split()[2],eachLine.split()[6]])
+    url = urlparse(eachLine.split()[6]).hostname
+    if type(url) != type(None):
+        url = ".".join(url.split(".")[::-1])
+        print "\t".join([eachLine.split()[0],eachLine.split()[2],url])

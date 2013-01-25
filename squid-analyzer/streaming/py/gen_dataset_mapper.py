e5a82328404f35282a0e48dba118540f88510075
Sidharth Shah <sidharth@fafadiatech.com>
2013-01-25 13:12:59 +0530

    now emmit day instead of unix epochs

--- squid-analyzer/streaming/py/gen_dataset_mapper.py
+++ squid-analyzer/streaming/py/gen_dataset_mapper.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 
 import sys 
+import datetime 
+
+def unix2dt(offset):
+    """
+    Converts unix time stamps to python date time
+    """
+    return datetime.datetime.fromtimestamp(float(offset))
+
 from urlparse import urlparse
 for eachLine in sys.stdin:
     eachLine = eachLine.strip() 
     url = urlparse(eachLine.split()[6]).hostname
     if type(url) != type(None):
         url = ".".join(url.split(".")[::-1])
-        print "\t".join([eachLine.split()[0],eachLine.split()[2],url])
+        if len(url.split(".")[0]) == 2 and len(url.split(".")[1]) == 2:
+            url = ".".join(url.split(".")[:3])
+        else:
+            url = ".".join(url.split(".")[:2])
+        print "\t".join([str(unix2dt(float(eachLine.split()[0])).date()),eachLine.split()[2],url])
